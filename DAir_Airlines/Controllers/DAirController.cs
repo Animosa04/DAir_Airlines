5c7dfba3a98accfbe1bb32ac4e1adca076328fef
Animosa04 <deyana.atanasova04@gmail.com>
2023-10-31 20:23:04 +0100

    Added controller

---
+++ DAir_Airlines/Controllers/DAirController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DAir_Airlines.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DAirController : Controller
+    {
+        private readonly ILogger<DAirController> _logger;
+
+        public DAirController(ILogger<DAirController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "FirstQuery")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
